85f5ebade07277ad3be390202e0d013046c75514
[felventh] <[feleventh@hotmail.com]>
2018-11-21 19:28:32 +0800

    template

---
+++ koa-template.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const views = require('koa-views')
+const path = require('path')
+const app = new Koa()
+
+app.use(views(path.join(__dirname, './view'), {extension: 'ejs'}))
+app.use(async (ctx) => {
+  let title = 'hello koa'
+  await ctx.render('index', {
+    title
+  })
+})
+app.listen(3000)
